b7f16ffdb03d034f600195fed551f9f96ef504f6
Soham Nagawanshi <ndsoham@gmail.com>
2020-12-15 11:10:23 -0600

    Add label and images

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Soham Nagawanshi on 12/15/20.
+//  Copyright © 2020 Soham Nagawanshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
